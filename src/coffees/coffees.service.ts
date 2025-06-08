65f4b8569ab2fecf7491812603fa037bef55eb8c
Precioussoul <habsoff@gmail.com>
2025-06-08 19:26:24 +0100

    feat dto and global pipes

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,6 +1,7 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Coffee } from './entity/coffee.entity';
 import { UpdateCofeeDto } from './dto/update-cofee.dto';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
 
 @Injectable()
 export class CoffeesService {
@@ -21,32 +22,42 @@ export class CoffeesService {
     const coffee = this.coffees.find((coffee) => coffee.id === id);
 
     if (!coffee) {
-      throw new HttpException(
-        `Coffee with id ${id} not found`,
-        HttpStatus.NOT_FOUND,
-      );
+      throw new NotFoundException(`Coffee with id ${id} not found`);
     }
 
     return coffee;
   }
 
-  create(createCoffeeDto: any): void {
-    this.coffees.push(createCoffeeDto);
+  create(createCoffeeDto: CreateCoffeeDto): Coffee {
+    const newCoffee: Coffee = {
+      id: Math.max(0, ...this.coffees.map((coffee) => coffee.id)) + 1,
+      ...createCoffeeDto,
+    };
+    this.coffees.push(newCoffee);
+    return newCoffee;
   }
 
-  update(id: number, coffee: UpdateCofeeDto): Coffee {
-    const index = this.coffees.findIndex((c) => c.id === id);
-    if (index === -1) {
-      throw new Error(`Coffee with id ${id} not found`);
+  update(id: number, updateCoffeeDto: UpdateCofeeDto): Coffee {
+    const coffeeIndex = this.coffees.findIndex((coffee) => coffee.id === id);
+    if (coffeeIndex === -1) {
+      throw new NotFoundException(`Coffee with id ${id} not found`);
     }
-    this.coffees[index] = coffee;
-    return coffee;
+    const coffee = this.coffees[coffeeIndex];
+    // Create a new object with the existing coffee properties and override with the DTO
+    const updatedCoffee = {
+      ...coffee,
+      ...updateCoffeeDto,
+      id, // Ensure the ID is preserved
+    };
+
+    this.coffees[coffeeIndex] = updatedCoffee;
+    return updatedCoffee;
   }
 
   delete(id: number): void {
     const index = this.coffees.findIndex((c) => c.id === id);
     if (index === -1) {
-      throw new Error(`Coffee with id ${id} not found`);
+      throw new NotFoundException(`Coffee with id ${id} not found`);
     }
     this.coffees.splice(index, 1);
   }
