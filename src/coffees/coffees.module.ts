a06feb18326ea7f1754921685683fe92465a8755
Precioussoul <habsoff@gmail.com>
2025-06-17 22:07:32 +0100

    feat: setup mongo with nestjs

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CoffeeSchema } from './entity/coffee.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Coffee', schema: CoffeeSchema }]),
+  ],
   controllers: [CoffeesController],
   providers: [CoffeesService],
 })
